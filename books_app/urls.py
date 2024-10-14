351e8d55ee2ec6b5a77b9edc0f84e3c3a65ce734
Kirillo1 <kirill.han011@gmail.com>
2024-10-14 21:31:03 +0300

    Реализован показ книг на главной странице и детальный показ отдельной книги.

--- books_app/urls.py
+++ books_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import view_books
+from .views import view_books, view_detail_book
 
 
 app_name = "books"
 
 urlpatterns = [
-    path('', view_books, name='index')
+    path('', view_books, name='index'),
+    path('detail_book/<int:book_id>/', view_detail_book, name='detail_book')
 ]
--- books_app/views.py
+++ books_app/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
+from .models import Book
+
 def view_books(request):
-    return render(request, "books/index.html")
+    books = Book.objects.all()
+    context = {
+        "books": books
+    }
+
+    return render(request, "books/index.html", context=context)
+
+
+def view_detail_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    context = {
+        "book": book
+    }
+    return render(request, "books/detail_book.html", context=context)
