54a876561c99f6624aaca1aa83396dba4a3ab950
Kirillo1 <kirill.han011@gmail.com>
2024-10-14 20:13:25 +0300

    Подключили БД PostgreSQL к проекту.

--- core/settings.py
+++ core/settings.py
@@ -83,9 +83,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": env("SQL_ENGINE"),
+        "NAME": env("SQL_DATABASE"),
+        "USER": env("SQL_USER"),
+        "PASSWORD": env("SQL_PASSWORD"),
+        "PORT": env("SQL_PORT"),
     }
 }
 
