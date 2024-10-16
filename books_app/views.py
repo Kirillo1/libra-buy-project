00b7b736c5ecf5d9a36368ed46080bf28add1679
Kirillo1 <kirill.han011@gmail.com>
2024-10-16 14:23:02 +0300

    Изменена функция представления и шаблон отображения детальной информации о книги.

--- books_app/views.py
+++ books_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Book
 
@@ -12,8 +12,10 @@ def view_books(request):
 
 
 def view_detail_book(request, book_id):
-    book = Book.objects.get(id=book_id)
+    book = get_object_or_404(Book, id=book_id)
+    genres = book.genres.all()
     context = {
-        "book": book
+        "book": book,
+        "genres": genres
     }
     return render(request, "books/detail_book.html", context=context)
