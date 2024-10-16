d8b27d10a8a6b72e95638acdee0bd9e147acab22
Kirillo1 <kirill.han011@gmail.com>
2024-10-16 14:58:04 +0300

    Добавлены классы к формам.

--- books_app/forms.py
+++ books_app/forms.py
@@ -5,3 +5,12 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['name', 'author', 'genres', 'description', 'publication', 'publication_year', 'price']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
+            'genres': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'publication': forms.TextInput(attrs={'class': 'form-control'}),
+            'publication_year': forms.NumberInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
