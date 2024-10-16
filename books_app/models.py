200c5d60dd0db2254c265382eff47ea736e7a37b
Kirillo1 <kirill.han011@gmail.com>
2024-10-16 14:11:54 +0300

    Изменена модель Book. Создана и применена миграция.

--- books_app/models.py
+++ books_app/models.py
@@ -1,30 +1,51 @@
 from django.db import models
 
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Жанр"
+    )
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+
 class Book(models.Model):
     name = models.CharField(
-        max_length=200
+        max_length=200,
+        verbose_name='Название'
     )
     author = models.CharField(
-        max_length=100
+        max_length=100,
+        verbose_name='Автор'
     )
-    genre = models.CharField(
-        max_length=100
+    genres = models.ManyToManyField(
+        Genre, 
+        verbose_name='Жанры'
     )
     description = models.TextField(
-        max_length=500
+        max_length=500,
+        verbose_name='Описание'
     )
     publication = models.CharField(
-        max_length=100
+        max_length=100,
+        verbose_name='Издательство'
     )
-    publication_year = models.CharField(
-        max_length=4
+    publication_year = models.IntegerField(
+        verbose_name='Год издания'
     )
     price = models.DecimalField(
         max_digits=10,
-        decimal_places=2
+        decimal_places=2,
+        verbose_name='Цена'
     )
     create_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name='Дата добавления'
     )
 
     def __str__(self):
