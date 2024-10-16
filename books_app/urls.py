cb1f535666b7507dbee93e1559260f4c3c4a4378
Kirillo1 <kirill.han011@gmail.com>
2024-10-16 14:56:53 +0300

    Реализовано добавление, удаление и изменение книг. Добавлены шаблоны.

--- books_app/urls.py
+++ books_app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import view_books, view_detail_book
+from .views import (view_books, view_detail_book, 
+                    add_book, delete_book, edit_book)
 
 
 app_name = "books"
 
 urlpatterns = [
     path('', view_books, name='index'),
-    path('detail_book/<int:book_id>/', view_detail_book, name='detail_book')
+    path('detail_book/<int:book_id>/', view_detail_book, name='detail_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book')
+
 ]
--- books_app/views.py
+++ books_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Book
+from .forms import BookForm
+
 
 def view_books(request):
     books = Book.objects.all()
@@ -19,3 +21,35 @@ def view_detail_book(request, book_id):
         "genres": genres
     }
     return render(request, "books/detail_book.html", context=context)
+
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books:index')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
+
+
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('books:index')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/add_book.html', {'form': form})
+
+
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    print(book)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('books:index')
+    return render(request, 'books/delete_book.html', {'book': book})
