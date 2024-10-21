160f996c558cca4f674bb28cdd9792081944b1e9
Kirillo1 <kirill.han011@gmail.com>
2024-10-21 14:13:01 +0300

    Добавлена модель комментария. Создана и применена миграция.

--- comments_app/models.py
+++ comments_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from books_app.models import Book
 
-# Create your models here.
+
+class Comment(models.Model):
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    email = models.EmailField(
+        verbose_name='Email'
+    )
+    text = models.TextField(
+        verbose_name='Комментарий'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Комментарий от {self.email} к книге "{self.book.name}"'
