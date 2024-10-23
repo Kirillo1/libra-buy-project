abc76db7c1c48c6e2808accdf335ad6f81d474bc
Kirillo1 <kirill.han011@gmail.com>
2024-10-23 10:55:47 +0300

    Добавлена модель пользователя. Созданы и применены миграции.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class User(AbstractUser):
+    phone_number = PhoneNumberField(
+        'Номер телефона',
+        unique=True,
+    )
+    father_name = models.CharField(
+        'Отчество',
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(
+        'Ваша фотография',
+        upload_to='users/',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
