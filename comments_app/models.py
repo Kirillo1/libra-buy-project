5ef5bf4dbfcf24d22dcfed6f165f2747005fc70f
Kirillo1 <kirill.han011@gmail.com>
2024-10-30 16:05:04 +0300

    Изменена модель комментария. Создана и применена миграция

--- comments_app/models.py
+++ comments_app/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 from books_app.models import Book
 
 
+User = get_user_model()
+
 class Comment(models.Model):
     book = models.ForeignKey(
-        Book, on_delete=models.CASCADE,
+        Book, 
+        on_delete=models.CASCADE,
         related_name='comments'
+    )
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='comments',
+        verbose_name='Автор'
     )
     email = models.EmailField(
         verbose_name='Email'
@@ -16,6 +29,10 @@ class Comment(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Дата создания'
+    )
+    is_verified = models.BooleanField(
+        default=False,
+        verbose_name='Проверен администратором'
     )
 
     class Meta:
