acaf4e256c737e4201646e03678cd2c5acb908dc
Kirillo1 <kirill.han011@gmail.com>
2024-11-05 12:24:36 +0300

    Правка файлов.

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,10 @@
 import { updateBookStatus, updateCommentStatus } from "./admin.js";
+import { likeButtonsHandler, highlightStars } from "./books_view.js";
 
 
 document.addEventListener("DOMContentLoaded", function () {
+    likeButtonsHandler();
+    highlightStars();
     updateBookStatus();
     updateCommentStatus();
 });
