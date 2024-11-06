7ac7d808c7e6941a507920c2a960af4680dc61ed
Kirillo1 <kirill.han011@gmail.com>
2024-11-06 11:09:37 +0300

    Добавлена модель корзины. Создана и применена миграция.

--- cart_app/models.py
+++ cart_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from books_app.models import Book
+
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='cart'
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(
+        default=1
+    )
+
+    class Meta:
+        unique_together = ('user', 'book')
+        verbose_name = "КОрзина"
+        verbose_name_plural = "Корзины"
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.name} (x{self.quantity})"
