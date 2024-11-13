cdd4371018538a137c4aef42d9adafeb1e2ec28e
Kirillo1 <kirill.han011@gmail.com>
2024-11-13 15:32:36 +0300

    Выполнен рефакторинг кода и добавлены типы в приложение users

--- users/views.py
+++ users/views.py
@@ -2,26 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db import transaction
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 
 from books_app.models import Book
 from comments_app.models import Comment
-from order_app.models import OrderItem, Order
+from order_app.models import Order
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from .utils import is_admin
 
 
 @login_required(login_url='users:login')
-def view_profile(request):
+def view_profile(request: HttpRequest) -> HttpResponse:
     user_books = Book.objects.filter(seller=request.user)
     return render(request, 'users/profile.html', {'user_books': user_books})
 
 
-def is_admin(user):
-    return user.is_authenticated and user.is_staff
-
-
 @login_required(login_url='users:login')
 @user_passes_test(is_admin)
-def view_admin_dashboard(request):
+def view_admin_dashboard(request: HttpRequest) -> HttpResponse:
     books = Book.objects.filter(is_verified=False)
     comments = Comment.objects.filter(is_verified=False)
     orders = Order.objects.filter(payment_status='unpaid')
@@ -36,7 +34,7 @@ def view_admin_dashboard(request):
 
 
 @transaction.atomic
-def register_user_view(request):
+def register_user_view(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -48,7 +46,7 @@ def register_user_view(request):
     return render(request, 'users/register.html', {'form': form})
 
 
-def login_view(request):
+def login_view(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = CustomAuthenticationForm(data=request.POST)
         if form.is_valid():
@@ -63,7 +61,7 @@ def login_view(request):
     return render(request, 'users/login.html', {'form': form})
 
 
-def logout_view(request):
+def logout_view(request: HttpRequest) -> HttpResponseRedirect:
     logout(request)
 
     return redirect('books:index')
